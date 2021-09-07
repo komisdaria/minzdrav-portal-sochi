8dfd5884b7db28fd4a61f667f8e16ff310fb2126
komisdaria <darijana701@gmail.com>
2021-09-07 18:48:53 +0300

    some styles and components

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,44 +1,45 @@
 import React from 'react'
 import 'antd/dist/antd.css';
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Typography } from 'antd';
+
 
 const Login = () => {
   
+  const { Title } = Typography;
 
   return (
+    <div className='auth'>
+      <Title type="success" level={3}>Вход в личный кабинет</Title>
     <Form
       name="basic"
       labelCol={{ span: 8 }}
-      wrapperCol={{ span: 10 }}
+      wrapperCol={{ span: 8 }}
       initialValues={{ remember: true }}
       autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
       >
+      <Form.Item
+        label="Почта"
+        name="username"
+        rules={[{ required: true, message: 'Введите электронную почту' }]}
+        >
         <Input />
       </Form.Item>
 
       <Form.Item
-        label="Password"
+        label="Пароль"
         name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
+        rules={[{ required: true, message: 'Введите пароль' }]}
+        >
         <Input.Password />
       </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
+      <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
+        <Button type="primary" htmlType="submit" >
+          Войти
         </Button>
       </Form.Item>
     </Form>
+        </div>
   );
 };
 
--- frontend/src/components/Registration/Registration.tsx
+++ frontend/src/components/Registration/Registration.tsx
@@ -1,43 +1,63 @@
-import React from 'react'
-import 'antd/dist/antd.css';
-import { Form, Input, Button, Checkbox } from 'antd';
+import React from "react";
+import "antd/dist/antd.css";
+import { Form, Input, Button, Typography } from 'antd';
+
 
 const Registration = () => {
-  
+
+  const { Title } = Typography;
+
   return (
+    <div className='auth'>
+      <Title type="success" level={3}>Регистрация</Title>
+
     <Form
       name="basic"
       labelCol={{ span: 8 }}
-      wrapperCol={{ span: 10 }}
+      wrapperCol={{ span: 8 }}
       initialValues={{ remember: true }}
       autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
       >
+      <Form.Item
+        label="Почта"
+        name="username"
+        rules={[{ required: true, message: "Введите электронную почту" }]}
+        >
         <Input />
       </Form.Item>
 
       <Form.Item
-        label="Password"
+        label="Номер полиса ОМС"
+        name="username"
+        rules={[{ required: true, message: "Введите номер полиса ОМС" }]}
+        >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Пароль"
         name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
+        rules={[{ required: true, message: "Введите пароль" }]}
+        >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        label="Повторите пароль"
+        name="password"
+        rules={[{ required: true, message: "Повторите пароль" }]}
+        >
         <Input.Password />
       </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
+
+      <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
+        <Button type="primary" htmlType="submit" >
+          Зарегистрироваться
         </Button>
       </Form.Item>
     </Form>
+        </div>
   );
 };
 
