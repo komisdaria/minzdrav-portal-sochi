b897cbf9ef8182514df0b4ef8f399acf126321d2
Zeus <maver176005@mail.ru>
2021-09-09 16:53:01 +0300

    merge

--- frontend/src/components/Doctors/Doctors.tsx
+++ frontend/src/components/Doctors/Doctors.tsx
@@ -4,41 +4,6 @@ import { useDispatch } from "react-redux";
 import { getDoctorAC } from "../../redux/ActionCreators/UserAC/getDoctor";
 import Spinner from "../Spinner/Spinner";
 
-// const dbDoc = [
-//   {
-//     name: "Калинов Александр Васильевич",
-//     specialization: "Терапевт",
-//     work: [9 - 18],
-//     raiting: 4.6,
-//     reviews: ["быстро принимает", "настоящий профи медицины"],
-//     id: "1",
-//   },
-//   {
-//     name: "Денисова Марина Дмитриевна",
-//     specialization: "Терапевт",
-//     work: [10 - 16],
-//     raiting: 4.8,
-//     reviews: ["отличный врач"],
-//     id: "2",
-//   },
-//   {
-//     name: "Калинов Александр Васильевич",
-//     specialization: "Ортопед",
-//     work: [12 - 14],
-//     raiting: 4.1,
-//     reviews: ["принимает долго"],
-//     id: "3",
-//   },
-//   {
-//     name: "Миронова Снеженна Денисовна",
-//     specialization: "Хирург",
-//     work: [16 - 20],
-//     raiting: 3.2,
-//     reviews: ["справку в санаторий не дает"],
-//     id: "4",
-//   },
-// ];
-
 const Doctors = () => {
   // const [doctors, setDoctors] = useState(dbDoc);
   const doctorsState = useMySelector((state) => state.doctors);
