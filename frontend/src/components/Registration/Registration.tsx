8d8ed984befabf90880a456efdfd60f2a7da7267
Alexey <sologod@yandex.ru>
2021-09-15 19:05:38 +0300

    fix footer

--- frontend/src/components/Registration/Registration.tsx
+++ frontend/src/components/Registration/Registration.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { CreateUserAC } from "../../redux/ActionCreators/UserAC/createUserAC";
 import { useHistory } from "react-router";
+import css from "./registration.module.css";
 
 const Registration = () => {
   const { Title } = Typography;
@@ -46,76 +47,78 @@ const Registration = () => {
   };
 
   return (
-    <div className="auth">
-      <Title type="success" level={3}>
-        Регистрация
-      </Title>
+    <div className={css.wrap}>
+      <div className="auth">
+        <Title type="success" level={3}>
+          Регистрация
+        </Title>
 
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 8 }}
-        initialValues={{ remember: true }}
-        autoComplete="off"
-        onSubmitCapture={onSubmit}
-      >
-        <Form.Item
-          label="Имя"
-          name="name"
-          rules={[{ required: true, message: "Введите ФИО" }]}
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 8 }}
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          onSubmitCapture={onSubmit}
         >
-          <Input onChange={nameHandler} value={name} />
-        </Form.Item>
+          <Form.Item
+            label="Имя"
+            name="name"
+            rules={[{ required: true, message: "Введите ФИО" }]}
+          >
+            <Input onChange={nameHandler} value={name} />
+          </Form.Item>
 
-        <Form.Item
-          label="Почта"
-          name="email"
-          rules={[{ required: true, message: "Введите электронную почту" }]}
-        >
-          <Input onChange={emailHandler} value={email} />
-        </Form.Item>
+          <Form.Item
+            label="Почта"
+            name="email"
+            rules={[{ required: true, message: "Введите электронную почту" }]}
+          >
+            <Input onChange={emailHandler} value={email} />
+          </Form.Item>
 
-        <Form.Item
-          label="Номер полиса ОМС"
-          name="oms"
-          rules={[
-            {
-              required: true,
-              message: "Введите номер полиса ОМС",
-              min: 16,
-              max: 16,
-            },
-          ]}
-        >
-          <Input
-            onChange={omsHandler}
-            type="number"
-            value={oms}
-            maxLength={16}
-          />
-        </Form.Item>
+          <Form.Item
+            label="Номер полиса ОМС"
+            name="oms"
+            rules={[
+              {
+                required: true,
+                message: "Введите номер полиса ОМС",
+                min: 16,
+                max: 16,
+              },
+            ]}
+          >
+            <Input
+              onChange={omsHandler}
+              type="number"
+              value={oms}
+              maxLength={16}
+            />
+          </Form.Item>
 
-        <Form.Item
-          label="Пароль"
-          name="password"
-          rules={[{ required: true, message: "Введите пароль" }]}
-        >
-          <Input.Password onChange={passwordHandler} value={password} />
-        </Form.Item>
-        <Form.Item
-          label="Повторите пароль"
-          name="passwordrepit"
-          rules={[{ required: true, message: "Повторите пароль" }]}
-        >
-          <Input.Password onChange={repPasswordHandler} value={repPassword} />
-        </Form.Item>
+          <Form.Item
+            label="Пароль"
+            name="password"
+            rules={[{ required: true, message: "Введите пароль" }]}
+          >
+            <Input.Password onChange={passwordHandler} value={password} />
+          </Form.Item>
+          <Form.Item
+            label="Повторите пароль"
+            name="passwordrepit"
+            rules={[{ required: true, message: "Повторите пароль" }]}
+          >
+            <Input.Password onChange={repPasswordHandler} value={repPassword} />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-          <Button type="primary" htmlType="submit">
-            Зарегистрироваться
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
+            <Button type="primary" htmlType="submit">
+              Зарегистрироваться
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
