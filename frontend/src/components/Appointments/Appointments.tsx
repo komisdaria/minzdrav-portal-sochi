95c30b478995d328b00b75eeaabab7befae87578
Zeus <maver176005@mail.ru>
2021-09-15 17:16:01 +0300

    fixPlagination

--- frontend/src/components/Appointments/Appointments.tsx
+++ frontend/src/components/Appointments/Appointments.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useMySelector } from "../../hooks/customHook";
 import { getAppointmentsAC } from "../../redux/ActionCreators/AppointmentsAC/AppointmentsAC";
-import { Card } from "antd";
+import { Card, Pagination } from "antd";
 import styles from "./appointments.module.css";
 import { Select } from "antd";
 import { AppointmentType } from "../../userTypes/appointmentType";
@@ -11,15 +11,24 @@ import { idText } from "typescript";
 import { updateStatusAppoinmentAC } from "../../redux/ActionCreators/AppointmentsAC/updateStatusAppointmentAC";
 
 const Appointments = () => {
-
   const appointmentsState = useMySelector((state) => state.appointments);
   const dispatch = useDispatch();
-  const [search, setSearch] = useState('');
-  const [findSpecial, setFindSpecial] = useState<AppointmentType[]>([])
+  const [search, setSearch] = useState("");
+  const [pageSize, setPageSize] = useState(15);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [findSpecial, setFindSpecial] = useState<AppointmentType[]>([]);
 
-  console.log('STATE=',appointmentsState);
+  const totalPage = appointmentsState.length / pageSize;
+  const currentPageData = appointmentsState.slice(
+    (pageNumber - 1) * pageSize,
+    (pageNumber - 1) * pageSize + pageSize
+  );
+  const currentPageData1 = findSpecial.slice(
+    (pageNumber - 1) * pageSize,
+    (pageNumber - 1) * pageSize + pageSize
+  );
 
-
+  console.log("STATE=", appointmentsState);
 
   const { Option } = Select;
 
@@ -29,29 +38,28 @@ const Appointments = () => {
     setSearch(value);
   }
 
-  const handleKey = (event: { key: string; }) => {
+  const handleKey = (event: { key: string }) => {
     if (event.key === "Enter") {
-      const filteredSpecial = appointmentsState.filter((el) => el.doctorSpecialization === search)
-      console.log('filteredSpecial', filteredSpecial);
+      const filteredSpecial = appointmentsState.filter(
+        (el) => el.doctorSpecialization === search
+      );
+      console.log("filteredSpecial", filteredSpecial);
       setFindSpecial(filteredSpecial);
-
     }
   };
   useEffect(() => {
     dispatch(getAppointmentsAC());
   }, [dispatch]);
 
-  const reservedAppoint = (id: AppointmentType['id']) => {
-    dispatch(addToUserAppointmentAC(id))
-    dispatch(updateStatusAppoinmentAC(id))
-  }
+  const reservedAppoint = (id: AppointmentType["id"]) => {
+    dispatch(addToUserAppointmentAC(id));
+    dispatch(updateStatusAppoinmentAC(id));
+  };
 
   return (
     <div>
       <h1>Для вашего удобства выберите нужного специалиста</h1>
-      <form
-        onKeyDown={handleKey}
-      >
+      <form onKeyDown={handleKey}>
         <Select
           showSearch
           style={{ width: 350 }}
@@ -60,12 +68,13 @@ const Appointments = () => {
           onChange={onChange}
           // onSearch={onSearch}
           filterOption={(input, option: any) => {
-            if (typeof option.value === 'string') {
-              return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            if (typeof option.value === "string") {
+              return (
+                option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              );
             }
-            return false
-          }
-          }
+            return false;
+          }}
         >
           <Option value="Терапевт">Терапевт</Option>
           <Option value="Ортопед">Ортопед</Option>
@@ -74,44 +83,76 @@ const Appointments = () => {
           <Option value="Офтальмолог">Офтальмолог</Option>
           <Option value="Акушер - гинеколог">Акушер - гинеколог</Option>
         </Select>
-
       </form>
 
       <div className={styles.cardwrapper}>
-        {findSpecial.length ? (
-          findSpecial.map((appoint: { id: string; date: string, time: string, doctorSpecialization: string, status: boolean }) => (
-            <div key={appoint.id} >
-              <Card className={`${appoint.status ? styles.appoint_item : ''}`}
-                title={`Запись к врачу: ${appoint.doctorSpecialization}`}
-                bordered={true}
-                style={{ width: 300 }}
-                
-              >
-                <p >Дата: {appoint.date}</p>
-                <p>Время: {appoint.time}</p>
-                {appoint.status ? <div>Время забронировано</div> :
-                <button onClick={() => reservedAppoint(appoint.id)}>Записаться на прием</button>
-                }
-              </Card>
-            </div>
-          ))
-        ) : (
-          appointmentsState.map((appoint: { id: string; date: string, time: string, doctorSpecialization: string, status: boolean }) => (
-            <div key={appoint.id} className={`${appoint.status ? styles.appoint_item : ''}`}>
-              <Card
-                title={`Запись к врачу: ${appoint.doctorSpecialization}`}
-                bordered={true}
-                style={{ width: 300 }}
-              >
-                <p>Дата: {appoint.date}</p>
-                <p>Время: {appoint.time}</p>
-                {appoint.status ? <div>Время забронировано</div> :
-                <button onClick={() => reservedAppoint(appoint.id)}>Записаться на прием</button>
-                } 
-              </Card>
-            </div>
-          ))
-        )}
+        {currentPageData1.length
+          ? currentPageData1.map(
+              (appoint: {
+                id: string;
+                date: string;
+                time: string;
+                doctorSpecialization: string;
+                status: boolean;
+              }) => (
+                <div key={appoint.id}>
+                  <Card
+                    className={`${appoint.status ? styles.appoint_item : ""}`}
+                    title={`Запись к врачу: ${appoint.doctorSpecialization}`}
+                    bordered={true}
+                    style={{ width: 300 }}
+                  >
+                    <p>Дата: {appoint.date}</p>
+                    <p>Время: {appoint.time}</p>
+                    {appoint.status ? (
+                      <div>Время забронировано</div>
+                    ) : (
+                      <button onClick={() => reservedAppoint(appoint.id)}>
+                        Записаться на прием
+                      </button>
+                    )}
+                  </Card>
+                </div>
+              )
+            )
+          : currentPageData.map(
+              (appoint: {
+                id: string;
+                date: string;
+                time: string;
+                doctorSpecialization: string;
+                status: boolean;
+              }) => (
+                <div
+                  key={appoint.id}
+                  className={`${appoint.status ? styles.appoint_item : ""}`}
+                >
+                  <Card
+                    title={`Запись к врачу: ${appoint.doctorSpecialization}`}
+                    bordered={true}
+                    style={{ width: 300 }}
+                  >
+                    <p>Дата: {appoint.date}</p>
+                    <p>Время: {appoint.time}</p>
+                    {appoint.status ? (
+                      <div>Время забронировано</div>
+                    ) : (
+                      <button onClick={() => reservedAppoint(appoint.id)}>
+                        Записаться на прием
+                      </button>
+                    )}
+                  </Card>
+                </div>
+              )
+            )}
+        <Pagination
+          defaultCurrent={1}
+          total={totalPage}
+          onChange={(page, pageSize = 10) => {
+            setPageNumber(page);
+            setPageSize(pageSize + 5);
+          }}
+        />
       </div>
     </div>
   );
